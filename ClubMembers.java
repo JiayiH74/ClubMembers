2f3fd6cc5b0ce2fa164539e38500b0e99122e9f0
JiayiH74 <jiayih12@nycstudents.net>
2025-05-06 11:22:36 -0400

    Create ClubMembers.java

---
+++ ClubMembers.java
@@ -0,0 +1,23 @@
+public class ClubMembers
+{
+  private ArrayList<MemberInfo> memberList;
+  public ClubMembers()
+  {
+  }
+
+  public void addMembers(String[] names, int gradYear)
+  {
+  }
+
+  public ArrayList<MemberInfo> getMemberList()
+  {
+  }
+
+  public ArrayList<MemberInfo> removeMembers(int year) 
+  {
+  }
+
+  public void setMemberList(ArrayList<MemberInfo> members)
+  {
+  }
+}
