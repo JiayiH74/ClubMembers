1b00108234f19a8a9fac99dcfb9bed625b802834
JiayiH74 <jiayih12@nycstudents.net>
2025-05-06 11:18:21 -0400

    Create MemberInfo.java

---
+++ MemberInfo.java
@@ -0,0 +1,23 @@
+public class MemberInfo
+  {
+    private String name;
+    private int gradYear;
+    private boolean hasGoodStanding;
+
+    public MemberInfo(String name, int gradYear, boolean hasGoodStanding)
+    {
+    }
+
+    public int getGradYear()
+    {
+      return gradYear;
+    }
+
+    public boolean inGoodStanding()
+    {
+    }
+
+    public String toString()
+    {
+    }
+  }
